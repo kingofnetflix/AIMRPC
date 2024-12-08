73de248427244b2c29b408aa09b1e42b6fb38022
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2024-12-08 12:10:13 -0600

    Add project files.

---
+++ AIMRPC/Libs/ProcessHandler.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Management;
+
+namespace AIMRPC.Libs
+{
+    #pragma warning disable CA1416 // fuck other platforms, just use windows
+    internal class ProcessHandler
+    {
+        public static Process? aim;
+        public static bool InitilizeAIM()
+        {
+            Process[] aims = Process.GetProcessesByName("aim");
+            if (aims.Length > 0)
+            {
+                aim = aims[0];
+                return true;
+            }
+            else if (aims.Length < 0)
+            {
+                aim = null;
+                return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ AIMRPC/Program.cs
@@ -0,0 +1,64 @@
+﻿using AIMRPC.Libs;
+
+namespace AIMRPC
+{
+    class Program
+    {
+        static bool compatThing = false;
+        private static void Init()
+        {
+            ProcessHandler.InitilizeAIM();
+            RPC.Init();
+            Config.ReadConfig();
+            Debug.Log("AIMRPC has been started!", Debug.Level.MESSAGE);
+        }
+        public static void Main()
+        {
+            Init();
+            while (true)
+            {
+                if (AIMHandler.AIMRunningInCompat() && compatThing)
+                {
+                    Debug.Log("AIM is running in Compatability Mode, you will not gain any support!", Debug.Level.FATAL);
+                    compatThing = true;
+                }
+                if (AIMHandler.IsAIMOpened())
+                {
+                    AIMHandler.SetScreenName();
+                    switch (AIMHandler.AIMActivity())
+                    {
+                        case AIMHandler.State.SIGNING_ON:
+                            RPC.UpdatePresence("Signing On..", string.Empty, RPC.aolguy, AIMHandler.screenName);
+                            break;
+                        case AIMHandler.State.BUDDYLIST:
+                            RPC.UpdatePresence("Looking at Buddy List", string.Empty, RPC.aolguy, AIMHandler.screenName);
+                            break;
+                        case AIMHandler.State.CHATTING:
+                            RPC.UpdatePresence(AIMHandler.ReturnCurrentBuddy(), string.Empty, RPC.aolguy, AIMHandler.screenName, RPC.buddy);
+                            break;
+                        case AIMHandler.State.PREFERENCES:
+                            RPC.UpdatePresence("Looking at Preferences", string.Empty, RPC.aolguy, AIMHandler.screenName);
+                            break;
+                        case AIMHandler.State.AVAILABLE:
+                            RPC.UpdatePresence("Available", string.Empty, RPC.aolguy, AIMHandler.screenName);
+                            break;
+                        case AIMHandler.State.EDITING_PROFILE:
+                            RPC.UpdatePresence("Editing their Profile", string.Empty, RPC.aolguy, AIMHandler.screenName);
+                            break;
+                        case AIMHandler.State.INVITING_TO_CHAT:
+                            RPC.UpdatePresence("Inviting people to Chat", string.Empty, RPC.aolguy, AIMHandler.screenName, RPC.buddy);
+                            break;
+                        case AIMHandler.State.NEUTRAL:
+                            RPC.EmptyPresence();
+                            break;
+                    }
+                }
+                else
+                {
+                    RPC.ClearPresence();
+                }
+            }
+        }
+    }
+}
+
